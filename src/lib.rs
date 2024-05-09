11cc5b8b374b785fad71db61010694affceb8efa
Max B <maxi.balabanski@gmail.com>
2024-05-09 17:10:03 -0400

    add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -15,17 +15,30 @@ pub struct Client
 
 impl Client
 {
+    /// Creates a new functions client
     pub fn new<T>(base_url: T) -> Client where T : Into<String>
     {
         Client { builder: Builder::new(base_url) }
     }
 
+
+    /// Authenticates a client using a JSON Web Token (JWT)
+    /// 
+    /// ## Usage
+    /// ```rs
+    /// let token = env!("JWT");
+    /// let client = Client::new(env!("SUPABASE_FUNCTION_URL")).auth(token);
+    /// ```
     pub fn auth<T>(mut self, token: T) -> Self where T : Into<String>
     {
        self.builder = self.builder.auth(token);
        self
     }
 
+    ///
+    /// Processes an http request and returns a FunctionResponse<U>
+    /// 
+    /// 
     async fn process_http_request<U>(self, res: Result<reqwest::Response, reqwest::Error>) 
         -> Result<FunctionResponse<U>, Error> where U : DeserializeOwned
     {
@@ -60,6 +73,18 @@ impl Client
        }
     }
 
+    /// Calls a Supabase function with an empty request body
+    /// 
+    /// ## Example
+    /// ```rs
+    /// #[derive(serde::Deserialize)]
+    /// struct HelloResponse
+    /// {
+    ///     message: String
+    /// }
+    /// ...
+    /// let response: FunctionResponse<HelloResponse> = client.call("hello").await.unwrap();
+    /// ```
     pub async fn call<T, U>(self, function_name: T) -> Result<FunctionResponse<U>, Error> where T :Into<String>, U : DeserializeOwned
     {
        let res = Function::new(function_name).call(&self.builder).await;
@@ -68,6 +93,20 @@ impl Client
        
     }
 
+    /// Calls a Supabase function with a request body
+    /// 
+    /// ## Example
+    /// ```rs
+    /// #[derive(serde::Serialize)]
+    /// struct HelloRequest
+    /// {
+    ///    name: String
+    /// }
+    /// 
+    /// let request = HelloRequest{name: String::from("World")};
+    /// 
+    /// let f: functions::FunctionResponse<HelloMessage> = c.call_with_body("hello", req).await.unwrap();
+    /// ```
     pub async fn call_with_body<T, U, V>(self, function_name: T, function_data: V) 
         -> Result<FunctionResponse<U>, Error> 
         where T :Into<String>, U : serde::de::DeserializeOwned, V : serde::ser::Serialize
